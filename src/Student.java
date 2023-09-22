5a102db29834cfd779c5325acb8632f7d4f81c58
BT_1N3_16 <sample@bths.edu>
2023-09-22 09:22:52 -0400

    finished

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private int grade;
+
+    public Student(String n, int g) {
+        name = n;
+        grade = g;
+    }
+
+    public void introduce() {
+        System.out.println("Hello my name is " + name + "! and I am a student!");
+    }
+
+    public void studentInfo() {
+        System.out.print("I am in grade " + grade);
+    }
+
+}
---
+++ src/StudentRunner.java
@@ -0,0 +1,7 @@
+public class StudentRunner {
+    public static void main(String[] args) {
+        Student student1 = new Student("Jerome", 11);
+        student1.introduce();
+        student1.studentInfo();
+    }
+}
